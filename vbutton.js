6d4655589d295f58ce5e792be865262197abbcbc
Hadden89 <Raffaele370@yahoo.it>
2021-03-01 16:51:50 +0100

    Updated to 3.6.2165.32

--- vbutton.js
+++ vbutton.js
@@ -1,10 +1,13 @@
+// Tnx to Luetage which actually did this for me ^^
+// https://github.com/luetage
+
 (function () {
 
 function style() {
     const style = document.createElement('style');
     style.type = 'text/css';
     style.id = 'mvVivbtn';
-    style.innerHTML = `.vivaldi {position: relative;}`;
+    style.innerHTML = ``;
     document.getElementsByTagName('head')[0].appendChild(style);
 };
 
@@ -12,7 +15,7 @@ function mvVivbtn() {
         style();
         var btn = document.querySelector(".vivaldi");
         btn.setAttribute('tabindex', '-1');
-        var bar = document.querySelector(".toolbar-addressbar");
+        var bar = document.querySelector(".UrlBar.toolbar.toolbar-mainbar.toolbar-large");
         var div = document.createElement('div');
         div.classList.add('button-toolbar');
         bar.insertBefore(div, bar.firstChild);
